a3f1a9def6d27b3b1baf6034b02cb0b62ec7f834
Kazuki Onodera <luvsic02@gmail.com>
2019-04-02 10:45:07 +0900

    update

--- py/005_count_sort_diff.py
+++ py/005_count_sort_diff.py
@@ -28,10 +28,10 @@ def fe(df):
         di = df[c].value_counts().sort_index().diff().to_dict()
         feature[f'{PREF}_{c}'] = df[c].map(di)
     
-#    for i in [3,2,1]:
-#        for c in tqdm(df.columns):
-#            di = df[c].round(i).value_counts().sort_index().diff().to_dict()
-#            feature[f'{PREF}_{c}_r{i}'] = df[c].round(i).map(di)
+    for i in [3,2,1]:
+        for c in tqdm(df.columns):
+            di = df[c].round(i).value_counts().sort_index().diff().to_dict()
+            feature[f'{PREF}_{c}_r{i}'] = df[c].round(i).map(di)
     
     feature.iloc[:200000].to_pickle(f'../data/train_{PREF}.pkl')
     feature.iloc[200000:].reset_index(drop=True).to_pickle(f'../data/test_{PREF}.pkl')
