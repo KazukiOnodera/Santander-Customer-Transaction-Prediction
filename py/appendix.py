f30e800aedacd66d744389a61bf4439934525a2c
kazuki-onodera <kazuki.onodera@dena.com>
2019-05-30 16:25:00 +0900

    update

--- py/appendix.py
+++ py/appendix.py
@@ -32,14 +32,27 @@ df.set_index('SubmissionDate', inplace=True)
 
 ix = df.index.tolist()
 ax = df.Score.plot()
-ax.yaxis.tick_right()
 plt.xticks(ix[::3]+[ix[-1]], rotation='vertical', fontsize=10)
-plt.yticks(fontsize=10)
 plt.xlabel('SubmissionDate', fontsize=13)
+plt.yticks(fontsize=10)
+plt.ylim((0.898, 0.9270))
+ax.yaxis.tick_right()
+plt.yticks(np.arange(0.898, 0.928, 0.002), fontsize=13)
 plt.title('Public LB Score Progress', fontsize=15)
 
 plt.hlines(0.92425, min(ix), max(ix), "gold", linestyles='dashed')
 plt.hlines(0.90120, min(ix), max(ix), "silver", linestyles='dashed')
 plt.hlines(0.90116, min(ix), max(ix), "brown", linestyles='dashed')
 plt.tight_layout()
+
+# =============================================================================
+# all team last density
+# =============================================================================
+df = pd.read_csv('/Users/kazuki.onodera/Downloads/santander-customer-transaction-prediction-publicleaderboard.csv',
+                 parse_dates=['SubmissionDate'])
+
+df.sort_values('SubmissionDate', ascending=False, inplace=True)
+df.drop_duplicates('TeamId', keep='first', inplace=True)
+
+df.Score.plot(kind='hist', bins=200, title='Public LB Score Histogram')
 
